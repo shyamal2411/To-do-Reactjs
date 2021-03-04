741ac77fb54cd3495b1476d49b16226c6dc4de15
Shyamal <sgprajapati24@gmail.com>
2021-03-04 14:26:08 +0530

    33.48, unable to solve error

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,27 +1,36 @@
 import React, {useState} from 'react'
 import TodoFromList from './TodoForm'
-import {RiCloseCircle} from  'react-icons/ri';
+import { RiCloseCircle, RiCloseCircleLine } from  'react-icons/ri';
 import {TiEdit} from  'react-icons/ti';
 
 
-function Todo({todos,completeTodo}) {
+function Todo({todos,completeTodo,removeTodo}) {
     const [edit, setEdit] =useState({
         id:null,
         value: ''
-    })
+    });
 
 
     return todos.map((todo,index)=>(
-    <div className={todo.isComplete ? 'todo-row complete' : 'todo-row'} key={index}>
-        <div key={todo.id} onClick={()=> complete(todo.id)}>
+    <div
+     className={todo.isComplete ? 'todo-row complete' : 'todo-row'} 
+     key={index}
+     >
+        <div key={todo.id} onClick={() => completeTodo(todo.id)}>
         {todo.text}
         </div>
         <div className="icons">
-            <RiCloseCircle/>
-            <TiEdit/>
+            <RiCloseCircleLine 
+                onClick={() => removeTodo(todo.id)}
+                className="delete-icon"
+            />
+            <TiEdit
+                onClick={()=>setEdit({id:todo.id,value:todo.text})}
+                className="edit-icon"
+            />
         </div>
     </div>
-    ))
+    ));
 }
-
-export default Todo
+//31.59
+export default Todo;
