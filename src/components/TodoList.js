8a046ee9553029ce6e6afa110c0e8639d8d314dd
Shyamal <sgprajapati24@gmail.com>
2021-03-06 15:04:03 +0530

    Changed TagLine

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -44,7 +44,7 @@ function TodoList() {
 
     return (
         <div>
-            <h1>What's the plan for today?</h1>
+            <h1>What are we doing today?</h1>
             <TodoForm onSubmit={addTodo}/>
             <Todo
             todos={todos} 
