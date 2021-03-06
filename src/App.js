02e7d86d777a65408ec8461242202d86a4b0d295
Shyamal <sgprajapati24@gmail.com>
2021-03-06 15:04:54 +0530

    Fixed Working of app

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import TodoForm from './components/TodoForm';
 import TodoList from './components/TodoList';
 function App() {
   return (
-    <div className="To-Do">
+    <div className="todo-app">
      <TodoList/>
     
     </div>
